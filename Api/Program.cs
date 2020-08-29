2b397bcaf6d4ac18166e5f2356094045af657e87
clcl0n <patrik.f.balaz@outlook.com>
2020-08-29 11:34:48 +0000

    logger + webhostbuilder

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,26 +1,77 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
 
 namespace Api
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+	public class Program
+	{
+		public static int Main(string[] args)
+		{
+			Log.Logger = new LoggerConfiguration()
+				.MinimumLevel.Debug()
+				.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+				.Enrich.FromLogContext()
+				.WriteTo.Console()
+				.WriteTo.File(
+					formatter: new CompactJsonFormatter(),
+					path: Path.Join(
+						Directory.GetParent(
+							Directory.GetCurrentDirectory()
+						).FullName,
+						"Logs/api.log"
+					),
+					fileSizeLimitBytes: long.MaxValue,
+					rollOnFileSizeLimit: true,
+					retainedFileCountLimit: 3
+				)
+				.CreateLogger();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+			try
+			{
+				Log.Information("Starting web host");
+				CreateHostBuilder(args).Build().Run();
+				return 0;
+			}
+			catch (Exception exception)
+			{
+				Log.Fatal(exception, "Host terminated unexpectedly");
+				return 1;
+			}
+			finally
+			{
+				Log.CloseAndFlush();
+			}
+		}
+
+		public static IWebHostBuilder CreateHostBuilder(string[] args)
+		{
+			IWebHostBuilder webHostBuilder = new WebHostBuilder();
+
+			webHostBuilder.UseSerilog();
+			webHostBuilder.UseKestrel();
+			webHostBuilder.UseWebRoot(Directory.GetCurrentDirectory());
+			webHostBuilder.ConfigureAppConfiguration((context, builder) =>
+			   ConfigureConfiguration(context, builder)
+			);
+			webHostBuilder.UseStartup<Startup>();
+
+			return webHostBuilder;
+		}
+
+		private static void ConfigureConfiguration(
+			WebHostBuilderContext context,
+			IConfigurationBuilder builder
+		)
+		{
+			builder
+				.AddJsonFile("appsettings.json", false)
+				.AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", true);
+		}
+	}
 }
--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,64 +1,67 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net.Mime;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace Api
 {
-    public class Startup
-    {
-        public IConfiguration Configuration { get; }
-        private IWebHostEnvironment _environment;
+	public class Startup
+	{
+		public IConfiguration Configuration { get; }
+		public IWebHostEnvironment Environment;
 
-        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
-        {
-            Configuration = configuration;
-            _environment = environment;
-        }
+		public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+		{
+			Configuration = configuration;
+			Environment = environment;
+		}
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            if (_environment.IsDevelopment())
-            {
-                services.AddSwaggerGen();
-            }
-            services.AddControllers();
-        }
+		// This method gets called by the runtime.
+		// Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			if (Environment.IsDevelopment())
+			{
+				services.AddSwaggerGen();
+			}
+			services.AddControllers(options =>
+			{
+				// Global settings for every the controllers.
+				options.Filters.Add(new ProducesAttribute(MediaTypeNames.Application.Json));
+			});
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseReDoc(options =>
-                {
-                    options.SpecUrl("/swagger/v1/swagger.json");
-                });
-                app.UseSwagger();
-                app.UseSwaggerUI(options =>
-                {
-                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
-                });
-                app.UseDeveloperExceptionPage();
-            }
+		// This method gets called by the runtime.
+		// Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app)
+		{
+			if (Environment.IsDevelopment())
+			{
+				app.UseReDoc(options =>
+				{
+					options.SpecUrl("/swagger/v1/swagger.json");
+				});
+				app.UseSwagger();
+				app.UseSwaggerUI(options =>
+				{
+					options.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+				});
+				app.UseDeveloperExceptionPage();
+				app.UseSerilogRequestLogging();
+			}
 
-            app.UseRouting();
+			app.UseRouting();
 
-            app.UseAuthorization();
+			app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
-    }
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+	}
 }
